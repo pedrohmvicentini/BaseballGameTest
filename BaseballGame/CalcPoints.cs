1c1b9abcd558a22e50fb296d1ac457dc73eac645
Pedro Vicentini <pedro.vicentini@rabbot.co>
2023-06-15 19:49:45 -0300

    Solution

---
+++ BaseballGame/CalcPoints.cs
@@ -0,0 +1,39 @@
+﻿namespace BaseballGame
+{
+    internal class CalcPoints
+    {
+        public static int Calc(string[] ops)
+        {
+            List<int> points = new List<int>();
+
+            if (ops != null && ops.Length > 0)
+            {
+                foreach (string op in ops)
+                    GetValue(ref points, op);
+            }
+
+            return points.Sum();
+        }
+
+        private static void GetValue(ref List<int> points, string op)
+        {
+            if (int.TryParse(op, out int val))
+                points.Add(val);
+            else if (op.ToUpper() == "C")
+            {
+                if (points.Any())
+                    points.RemoveAt(points.Count - 1);
+            }
+            else if (op.ToUpper() == "D")
+            {
+                if (points.Any())
+                    points.Add(points[points.Count - 1] * 2);
+            }
+            else if (op.ToUpper() == "+")
+            {
+                if (points.Count > 2)
+                    points.Add(points[points.Count - 2] + points[points.Count - 1]);
+            }
+        }
+    }
+}
---
+++ BaseballGame/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using BaseballGame;
+
+try
+{
+    string[] ops = new string[] { "5", "2", "C", "D", "+" };
+
+    Console.WriteLine(CalcPoints.Calc(ops));
+
+
+    ops = new string[] { "5", "-2", "4", "C", "D", "9", "+", "+" };
+
+    Console.WriteLine(CalcPoints.Calc(ops));
+
+
+    ops = new string[] { "1", "C" };
+
+    Console.WriteLine(CalcPoints.Calc(ops));
+
+
+    ops = new string[] { "C" };
+
+    Console.WriteLine(CalcPoints.Calc(ops));
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Something get wrong. " + ex.ToString());
+}
